c511afadcdf3e3c7184c1dcc333f1eda90a92d3d
Robert Wiggins <51722811+RandomRobbieBF@users.noreply.github.com>
2020-04-16 15:24:04 +0100

    Update lootme.sh

--- linux/lootme.sh
+++ linux/lootme.sh
@@ -1,1 +1,12 @@
-HOLDER
+#!/bin/bash
+for files in `cat files.txt`; do
+        echo.
+        echo "<details><summary^>$files</summary>"
+        echo "```"
+        cat $files
+        echo "```"
+        echo "</details>"
+        echo.
+        echo.
+
+done
